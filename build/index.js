7816041894f2626683163f2c6ef542672e3b8737
MarwanAhmed25 <marwan4125881@gmail.com>
2022-04-30 05:10:21 +0200

    front and static files

--- build/index.js
+++ build/index.js
@@ -9,13 +9,11 @@ const cors_1 = __importDefault(require("cors"));
 const path_1 = __importDefault(require("path"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const body_parser_1 = __importDefault(require("body-parser"));
-const work_1 = __importDefault(require("./handlars/work"));
-const skill_1 = __importDefault(require("./handlars/skill"));
-const type_1 = __importDefault(require("./handlars/type"));
-const project_skill_1 = __importDefault(require("./handlars/project_skill"));
-const project_1 = __importDefault(require("./handlars/project"));
-const skill_2 = require("./models/skill");
-const project_2 = require("./models/project");
+const skill_1 = require("./models/skill");
+const project_1 = require("./models/project");
+const type_1 = require("./models/type");
+const project_skill_1 = require("./models/project_skill");
+const work_1 = require("./models/work");
 dotenv_1.default.config();
 //initial port and app
 const PORT = process.env.PORT || 5000;
@@ -32,17 +30,14 @@ app.use(express_1.default.static(p));
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}...`);
 });
+const skill_obj = new skill_1.Skill(), project_obj = new project_1.Project(), type_obj = new type_1.Type(), project_skill_obj = new project_skill_1.Project_skill(), work_obj = new work_1.Work();
 app.get('/', async (req, res) => {
-    const skill_obj = new skill_2.Skill();
     const skills = await skill_obj.index();
-    const project_obj = new project_2.Project();
     const projects = await project_obj.index();
-    res.render('index', { skills: skills, projects: projects });
+    const types = await type_obj.index();
+    const project_skills = await project_skill_obj.index();
+    const works = await work_obj.index();
+    res.render('index', { skills, projects, works, types, project_skills });
 });
-(0, work_1.default)(app);
-(0, project_1.default)(app);
-(0, skill_1.default)(app);
-(0, type_1.default)(app);
-(0, project_skill_1.default)(app);
 //export the app to use when importing the file
 exports.default = app;
